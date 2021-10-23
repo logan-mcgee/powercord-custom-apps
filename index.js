9d1edf947e6d8d96d5cf2ab9edb98eb367621506
logan <31740751+sadboilogan@users.noreply.github.com>
2021-10-23 23:04:42 +0100

    tweak: use regex for detecting function, should mean that this works between updates

--- index.js
+++ index.js
@@ -8,34 +8,40 @@ module.exports = class CustomActivity extends Plugin {
     super();
   }
 
-  startPlugin() {
+  startPlugin () {
     powercord.api.settings.registerSettings('custom-activity', {
       category: this.entityID,
       label: 'Custom Activities',
       render: Settings
     });
 
-    this.findAndModifyActivity()
+    this.findAndModifyActivity();
   }
 
-  async findAndModifyActivity() {
-    const getActivityUrl = await getAllModules((m) => m?.default?.toString().includes(`if(i.default.inTestModeForEmbeddedApplication(e))return i.default.testModeOriginURL;`))[0];
+  async findAndModifyActivity () {
+    const getActivityUrl = await getAllModules((m) =>
+      m?.default?.toString()?.match(/if\(.\.default\.inTestModeForEmbeddedApplication\(.\)\)return .\.default\.testModeOriginURL;/)
+    )[0];
 
     inject('custom-activity', getActivityUrl, 'default', (args, res) => {
-      if (!this.settings.get('enabled', false)) return res;
+      if (!this.settings.get('enabled', false)) {
+        return res;
+      }
 
       const activityId = args[0];
-      if (this.settings.get(`activity-${activityId}`, `https://${activityId}.discordsays.com`) === `https://${activityId}.discordsays.com`) return res;
+      if (this.settings.get(`activity-${activityId}`, `https://${activityId}.discordsays.com`) === `https://${activityId}.discordsays.com`) {
+        return res;
+      }
 
       this.log(`[ACTIVITY URL] Custom activity found - ${activityId} - wants ${this.settings.get(`activity-${activityId}`, null)}`);
       res = this.settings.get(`activity-${activityId}`, null);
 
       return res;
-    }, false); //? Ensure the code runs *after* the regular, as we want to return a custom url if the activityId matches.
+    }, false); // ? Ensure the code runs *after* the regular, as we want to return a custom url if the activityId matches.
   }
 
-  pluginWillUnload() {
+  pluginWillUnload () {
     uninject('custom-activity');
     powercord.api.settings.unregisterSettings('custom-activity');
   }
-}
+};
